c112067e9ac30c570804472ad604ccd3d0505923
meydoed <meydoed@gmail.com>
2023-10-06 01:18:16 +0700

    commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,17 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+        Cat cat = applicationContext.getBean("cat", Cat.class);
+
+        Cat cat1 = applicationContext.getBean("cat", Cat.class);
+
+        System.out.println(bean == bean1);
+        System.out.println(cat == cat1);
+
     }
 }
